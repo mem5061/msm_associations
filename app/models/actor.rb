cd9404349f99ac4614f5a47c262a088d272724f7
Megan McLaughlin <megan.mclaughlin@users.noreply.github.com>
2017-05-09 11:27:49 -0500

    completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,9 @@
 class Actor < ApplicationRecord
+
+has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
+has_many(:movies, :class_name => "Movie", :foreign_key =>"id", :through => :characters)
+
+# - name: must be present; must be unique in combination with dob
+validates :name, :presence => true, :uniqueness => { :scope => :dob }
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,9 @@
 class Character < ApplicationRecord
+
+belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
+belongs_to(:movie, :class_name => "Movie", :foreign_key => "movie_id")
+
+validates :movie_id, :presence => true
+validates :actor_id, :presence => true
+
 end
